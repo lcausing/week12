8e95502db4e3d9c122ee41edbf558926f84df67f
Louie <causingljbcit@gmail.com>
2022-11-22 11:18:22 -0800

    button container

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -55,6 +55,18 @@ function createAddPara()
 clickMeButton.addEventListener("click",createAddPara);
 
 function changeBGGreen(event){
+    console.log(event.target);
     event.target.classList.add("green-background")
+
+}
+
+const buttonContainer = document.querySelector(".buttonContainer");
+buttonContainer.addEventListener("mouseover",changeBGGreen)
+
+buttonContainer.addEventListener("mouseover",changeParaToColor)
+
+function changeParaToColor(event){
+    // console.log(event.target.textContent)
+    event.target.style.color = event.target.textContent;
 
 }
