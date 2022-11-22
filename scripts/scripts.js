5af6103096b77da5cbac19cd18e5dda5dee63aea
Louie <causingljbcit@gmail.com>
2022-11-22 10:40:16 -0800

    event listener that changes text on button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,33 @@ const clickMeButton = document.querySelector(".click-me");
 // console.log(clickMeButton)
 
 function handleClickMeButton(){
-    alert("You have clicked the button!");
+    alert("Clicked!");
     // clickMeButton.removeEventListener('click',handleClickMeButton)
 }
 
-clickMeButton.addEventListener('click',handleClickMeButton,{once:true})
+// clickMeButton.addEventListener('click',handleClickMeButton,{once:true})
+// This will make event listener only do once and then gets removed
 
+// clickMeButton.addEventListener('click',handleClickMeButton);
+function changeBGcolor()
+{
+    // document.body.style.backgroundColor = "pink" 
+    // This one just changes the background color of the body to pink
+
+    document.body.classList.add("pink")
+    //This will add a class from css that has the background color pink
+
+}
+clickMeButton.addEventListener('click',changeBGcolor);
+
+
+function changeText(){
+    if (clickMeButton.textContent === "Clicked")
+    clickMeButton.textContent = "Click Me!";
+    else if (clickMeButton.textContent === "Click Me!")
+    clickMeButton.textContent = "Clicked";
+    // This is the code for changing if the button is clicked
+}
+
+clickMeButton.addEventListener("click",changeText);
 
